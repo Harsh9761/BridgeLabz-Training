7fc73ebb8e3bb753e687774a794b7f0032b2547a
Harsh9761 <harshmathura8@gmail.com>
2025-12-20 22:37:15 +0530

    [Harshdeep Singh] Add. Code fixed

--- core-java-practice/gcr-codebase/arrays/Factor.java
+++ core-java-practice/gcr-codebase/arrays/Factor.java
@@ -3,7 +3,7 @@ public class Factor{
 	public static void main(String args[]){
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
-		int factor[] = new int[n];
+		int factor[] = new int[num];
 		int count  = 0;
 		for(int i = 1;i<=num;i++) {
 			if(num%i==0) {
