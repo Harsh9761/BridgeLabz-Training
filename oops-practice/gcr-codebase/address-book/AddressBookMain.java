18358b4b2a322fb7915431025fca63adfe835947
Harsh9761 <harshmathura8@gmail.com>
2026-01-17 10:48:05 +0530

    [Harshdeep Singh]Add. Addressbook code added

--- oops-practice/gcr-codebase/address-book/AddressBookMain.java
+++ oops-practice/gcr-codebase/address-book/AddressBookMain.java
@@ -2,7 +2,9 @@
 public class AddressBookMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to the AddressBook Program");
+		
 
 	}
 
